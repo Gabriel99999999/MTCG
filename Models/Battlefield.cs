da6a36d65b85e54d6cdd99d16e486c08c3863044
Gabriel <wiedermann.gabriel@gmail.com>
2022-12-05 12:28:43 +0100

    Revert "Grundgerüst steht"
    This reverts commit d2616ed5c40f2a5770fc1bea15d0f7c73441ea9b.

--- Models/Battlefield.cs
+++ Models/Battlefield.cs
@@ -33,17 +33,23 @@ namespace MTCG.Models
                 card2 = player2.Deck[random.Next(amount2)];
                 if(!SpecialRule(player1, player2, card1, card2))
                 {
-                    //is the fight influenced by the elements ?
+                    //Monster vs Monster ?
                     if (CompareCurrentFighter(card1, card2))
                     {
-                        notElementInfluencedF(player1, player2, card1, card2);
+                        //Monster vs Monster
+                        if (!MvsM(player1, player2))
+                            Console.WriteLine($"Draw because:\nPlayer1 had: {card1.Element} {card1.Name} Damage: {card1.Damage} and\nPlayer2 had {card2.Element} {card2.Name} Damage: {card2.Damage}");
                     }
                     else
                     {
-                        //fight which is influenced by the elements
-                        elementInfluencedF(player1, player2, card1, card2);
+                        //fight with minimum one spell
                     }
                 }
+                /*
+                //Remove the first card of the decks
+                player1.Deck.Remove(card1);
+                player2.Deck.Remove(card2);
+                */
 
                 round++;                
             }
@@ -54,9 +60,19 @@ namespace MTCG.Models
             else
                 return null;
         }
-        public bool CompareCurrentFighter(Card card1, Card card2)
+        public static bool CompareElements(ElementType element1, ElementType element2)
         {
-            if ( (card1 is MonsterCard && card2 is MonsterCard) || (card1.Element == card2.Element))
+            return false;
+        }
+
+        public bool CompareMonsterType()
+        {
+            return false;
+        }
+
+        public bool CompareCurrentFighter(Card Card1, Card Card2)
+        {
+            if (Card1 is MonsterCard && Card2 is MonsterCard)
                 return true;
 
             return false;
@@ -70,37 +86,46 @@ namespace MTCG.Models
 
             CardType[] spells = { CardType.WaterSpell, CardType.FireSpell, CardType.RegularSpell };
 
-            //Bedigungen wo card1 gewinnt
-            //Dragon vs Goblin || Wizzard vs Ork || WaterSpell vs Knight || Kraken vs spells || FireElve vs Dragon
-            if ( (card1.Name is CardType.Dragon && card2.Name is CardType.Goblin)                       ||
-                 (card1.Name is CardType.Wizzard && card2.Name is CardType.Ork)                         ||
-                 (card1.Name is CardType.WaterSpell && card2.Name is CardType.Knight)                   ||
-                 (card1.Name is CardType.Kraken && Array.Exists(spells, x => x == card2.Name)           ||
-                 (card1.Element is ElementType.Fire && card1.Name is CardType.Elve) && card2.Name is CardType.Dragon))
-            {
-                winCard = card1;
-            }
-            //Bedigungen wo card2 gewinnt
-            //Goblin vs Dragon || Ork vs Wizzard || Knight vs WaterSpell || spells vs Kraken ||Dragon vs FireElve
-            else if ( (card1.Name is CardType.Goblin && card2.Name is CardType.Dragon)                     ||
-                     (card1.Name is CardType.Ork && card2.Name is CardType.Wizzard)                        ||
-                     (card1.Name is CardType.Knight && card2.Name is CardType.WaterSpell)                  ||
-                     (Array.Exists(spells, spell => spell == card1.Name) && card2.Name is CardType.Kraken) ||
-                     (card1.Name is CardType.Dragon && (card2.Element is ElementType.Fire && card2.Name is CardType.Elve)) )
-            {
+            //Goblin vs Dragon
+            if (card1.Name is CardType.Goblin && card2.Name is CardType.Dragon)
                 winCard = card2;
-            }
+            else if(card1.Name is CardType.Wizzard && card2.Name is CardType.Ork)
+                winCard = card1;
+
+            //Wizzard vs Ork
+            if (card1.Name is CardType.Ork && card2.Name is CardType.Wizzard)
+                winCard = card2;
+            else if (card1.Name is CardType.Dragon && card2.Name is CardType.Goblin)
+                winCard = card1;
+
+            //Knight vs WaterSpell
+            if (card1.Name is CardType.Knight && card2.Name is CardType.WaterSpell)
+                winCard = card2;
+            else if (card1.Name is CardType.WaterSpell && card2.Name is CardType.Knight)
+                winCard = card1;
+
+            //Kraken vs a Spell
+            if (card1.Name is CardType.Kraken && Array.Exists(spells, x => x == card2.Name) )
+                winCard = card2;
+            else if (Array.Exists(spells, spell => spell == card1.Name) && card2.Name is CardType.Kraken)
+                winCard = card1;
+
+            //FireElves vs Dragons
+            if (card1.Name is CardType.Dragon && (card2.Element is ElementType.Fire && card2.Name is CardType.Elve) )
+                winCard = card2;
+            else if ( (card1.Element is ElementType.Fire && card1.Name is CardType.Elve) && card2.Name is CardType.Dragon)
+                winCard = card1;
 
             if(winCard is not null)
             {
                 if (winCard == card1)
                 {
-                    Console.WriteLine($"Player1 win with: {card1.Element} {card1.Name} Damage: {card1.Damage} against\nPlayer2 with: {card2.Element} {card2.Name} Damage: {card2.Damage}");
+                    Console.WriteLine($"Player1 hat mit {card1.Element} {card1.Name} Damage: {card1.Damage} gegen {card2.Element} {card2.Name} Damage: {card2.Damage} gewonnen");
                     player1.Deck.Add(card2);
                 }
                 else
                 {
-                    Console.WriteLine($"Player2 win with: {card2.Element} {card2.Name} Damage: {card2.Damage} against\nPlayer1 with: {card1.Element} {card1.Name} Damage: {card1.Damage}");
+                    Console.WriteLine($"Player2 hat mit {card2.Element} {card2.Name} Damage: {card2.Damage} gegen {card1.Element} {card1.Name} Damage: {card1.Damage} gewonnen");
                     player2.Deck.Add(card1);
                 }
                 return true;
@@ -109,54 +134,24 @@ namespace MTCG.Models
             return false;
         }
 
-        public void notElementInfluencedF(User player1, User player2, Card card1, Card card2)
+        public bool MvsM (User player1, User player2)
         {
+            Card card1 = player1.Deck[1];
+            Card card2 = player2.Deck[1];
+
             if (card1.Damage == card2.Damage)
-                Console.WriteLine($"Draw because:\nPlayer1 had: {card1.Element} {card1.Name} Damage: {card1.Damage} and\nPlayer2 had {card2.Element} {card2.Name} Damage: {card2.Damage}");
+                return false;
             if (card1.Damage > card2.Damage)
             {
-                Console.WriteLine($"Player1 win with: {card1.Element} {card1.Name} Damage: {card1.Damage} against\nPlayer2 with: {card2.Element} {card2.Name} Damage: {card2.Damage}");
+                Console.WriteLine($"Player1 hat mit {card1.Element} {card1.Name} Damage: {card1.Damage} gegen {card2.Element} {card2.Name} Damage: {card2.Damage} gewonnen");
                 player1.Deck.Add(card2);
             }
             else
             {
-                Console.WriteLine($"Player2 win with: {card2.Element} {card2.Name} Damage: {card2.Damage} against\nPlayer1 with: {card1.Element} {card1.Name} Damage: {card1.Damage}");
+                Console.WriteLine($"Player2 hat mit {card2.Element} {card2.Name} Damage: {card2.Damage} gegen {card1.Element} {card1.Name} Damage: {card1.Damage} gewonnen");
                 player2.Deck.Add(card1);
             }
-        }
-
-        public void elementInfluencedF(User player1, User player2, Card card1, Card card2)
-        {
-            int damage1 = card1.Damage;
-            int damage2 = card2.Damage;
-            //Bedingungen die card1 Damage verdoppeln und card2 Damage halbiert
-            //Water vs Fire || Fire vs Normal || Normal vs Water
-            if( (card1.Element is ElementType.Water) && (card2.Element is ElementType.Fire) ||
-                (card1.Element is ElementType.Fire) && (card2.Element is ElementType.Normal) ||
-                (card1.Element is ElementType.Normal) && (card2.Element is ElementType.Water) )
-            {
-                damage1 = damage1 * 2;
-                damage2 = damage2 / 2;  
-            }
-            //Bedingungen die card2 Damage verdoppeln und card1 Damage halbiert
-            else
-            {
-                damage1 = damage1 / 2;
-                damage2 = damage2 * 2;
-            }
-
-            if (card1.Damage == card2.Damage)
-                Console.WriteLine($"Draw because:\nPlayer1 had: {card1.Element} {card1.Name} Damage: {card1.Damage} => calculated Damage: {damage1} and\nPlayer2 had {card2.Element} {card2.Name} Damage: {card2.Damage} => calculated Damage: {damage1}");
-            if (damage1 > damage2)
-            {
-                Console.WriteLine($"Player1 win with: {card1.Element} {card1.Name} Damage: {card1.Damage} => calculated Damage: {damage1} against\nPlayer2: {card2.Element} {card2.Name} Damage: {card2.Damage} => calculated Damage: {damage2}");
-                player1.Deck.Add(card2);
-            }
-            else
-            {
-                Console.WriteLine($"Player2 win with: {card2.Element} {card2.Name} Damage: {card2.Damage} => calculated Damage: {damage2} against\nPlayer2: {card1.Element} {card1.Name} Damage: {card1.Damage} => calculated Damage: {damage1}");
-                player2.Deck.Add(card1);
-            }
+            return true;
         }
     }
 }
